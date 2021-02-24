21a43ffac4593d18726dc95e6facf5832f98ccad
Audrey <adorism80@gmail.com>
2021-02-24 14:34:18 -0500

    Renaming code folder to src

---
+++ src/pew_load_graph.py
@@ -0,0 +1,46 @@
+import import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pyreadstat
+import seaborn as sns
+
+w68spss, metaspss = pyreadstat.read_sav('./data/W68.sav', apply_value_formats = True, formats_as_category = True )
+
+fields = ['COVIDFOL_W68', 
+          'COVIDCOVER1_W68', 
+          'COVIDFACTS_b_W68', 
+          'COVIDNEWSCHNG_a_W68', 
+          'COVIDNEWSCHNG_c_W68', 
+          'COVIDNEWSCHNG_d_W68',
+          'COVIDNEWSCHNG_e_W68',
+          'COVIDINFODIFF_W68', 
+          'COVIDLOCINFO_W68',
+          'COVIDDEAL_W68', 
+          'COVIDPLANHRD_W68', 
+          'COVIDPLANTRUE_W68', 
+          'COVIDPLANWATCH_W68', 
+          'F_METRO', 
+          'F_EDUCCAT',
+          'F_PARTY_FINAL']
+
+facets = ['F_METRO', 
+          'F_EDUCCAT',
+          'F_PARTY_FINAL']
+
+answers = ['COVIDFOL_W68', 
+          'COVIDCOVER1_W68', 
+          'COVIDFACTS_b_W68', 
+          'COVIDNEWSCHNG_a_W68', 
+          'COVIDNEWSCHNG_c_W68', 
+          'COVIDNEWSCHNG_d_W68',
+          'COVIDNEWSCHNG_e_W68',
+          'COVIDINFODIFF_W68', 
+          'COVIDLOCINFO_W68',
+          'COVIDDEAL_W68', 
+          'COVIDPLANHRD_W68', 
+          'COVIDPLANTRUE_W68', 
+          'COVIDPLANWATCH_W68']
+
+df68 = pd.DataFrame(w68spss)
+
+Cross_Tab(df68, answers, facets)
